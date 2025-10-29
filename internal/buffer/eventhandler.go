dd074ce73813245af8e0af578834b4fc25e6c058
matthias314 <matthias314@posteo.net>
2025-10-28 21:47:58 -0400

    added `Loc.IsValid`

--- internal/buffer/eventhandler.go
+++ internal/buffer/eventhandler.go
@@ -195,7 +195,7 @@ func (eh *EventHandler) InsertBytes(start Loc, text []byte) {
 	e := &TextEvent{
 		C:         *eh.cursors[eh.active],
 		EventType: TextEventInsert,
-		Deltas:    []Delta{{text, start, Loc{0, 0}}},
+		Deltas:    []Delta{{text, start, Loc{-1, -1}}},
 		Time:      time.Now(),
 	}
 	eh.DoTextEvent(e, true)
